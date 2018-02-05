c72ed4d1a7f73865c0890b0e474db42d10796148
EJ Mercado <ej@ejmercado.com>
2018-02-06 00:54:35 +0800

    basic matching

--- src/Chat.php
+++ src/Chat.php
@@ -5,26 +5,67 @@ use Ratchet\ConnectionInterface;
 
 class Chat implements MessageComponentInterface {
     protected $clients;
+    protected $applicants;
+    protected $businesses;
 
     function __construct(){
         $this->clients = new \SplObjectStorage;
+        $this->applicants = [];
+        $this->businesses = [];
     }
 
     public function onOpen(ConnectionInterface $conn) {
         $this->clients->attach($conn);
-
         echo "New Connection!({$conn->resourceId})\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+        $userInfo = json_decode($msg);
+        $user = [];
+        $user['userInfo'] = $userInfo;
+        $user['from'] = $from;
+        if($userInfo->type === 1){
+            $toSendApplicant = [];
+            echo "Applicant";
+            array_push($this->applicants, $user);
+            foreach($this->businesses as $business){
+                foreach($business['userInfo']->requirements as $job){
+                    $score = 0;
+                    $matchingSkills = [];
+                    foreach($job->jobRequirements as $jr){
+                        foreach($userInfo->requirements as $skill){
+                            if($skill->skill === $jr->requirement && $skill->years_exp >= $jr->years_exp){
+                                $score++;
+                                array_push($matchingSkills, [
+                                    'jSkill' => $jr->requirement,
+                                    'aSkill' => $skill->skill,
+                                    'jYears' => $jr->years_exp,
+                                    'aYears' => $skill->years_exp
+                                ]);
+                            }
+                        }
+                    }
+                    if($score > 1){
+                        array_push($toSendApplicant, [
+                            'matchingSkills' => $matchingSkills,
+                            'business' => $business,
+                            'job' => $job
+                        ]);
+                    }
+                }
+            }
+            $from->send(json_encode([
+                'message' => 'newMatches',
+                'matchesCount' => count($toSendApplicant),
+                'matches' => $toSendApplicant
+            ]));
+        }
 
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                // The sender is not the receiver, send to each client connected
-                $client->send($msg);
+        if($userInfo->type === 2){
+            echo "Business";
+            array_push($this->businesses, $user);
+            foreach($this->applicants as $applicant){
+                $applicant['from']->send(json_encode($userInfo));
             }
         }
     }
@@ -32,6 +73,21 @@ class Chat implements MessageComponentInterface {
     public function onClose(ConnectionInterface $conn) {
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
+        foreach ($this->applicants as $inda => $a){
+            if($a['from'] === $conn){
+                unset($this->applicants[$inda]);
+                echo "Successfully unset $applicants index {" . $inda . "} \n";       
+                echo "\n";       
+            }
+        }
+    
+        foreach ($this->businesses as $indb => $b){
+            if($b['from'] === $conn){
+                unset($this->businesses[$indb]);
+                echo "Successfully unset businesses index {" . $indb . "} \n";
+                echo "\n";       
+            }
+        }
 
         echo "Connection {$conn->resourceId} has disconnected\n";        
     }
